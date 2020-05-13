a7d9982f30b2af26098754e7fbfc78465d21024b
aphexddb <aphexddb@gmail.com>
2020-05-12 22:25:32 -0500

    xleanup

--- odp.sh
+++ odp.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -ex
 
-odp -file /opt/msm/html/players.json -host minecraft.aphexddb.com -password YOUR_RCON_SECRET_HERE
+if [[ `id -nu` != "minecraft" ]];then
+  echo "Not minecraft user, exiting.."
+  exit 1
+fi
+
+odp -file /opt/msm/html/players.json-host minecraft.aphexddb.com -password YOUR_RCON_SECRET_HERE
+
